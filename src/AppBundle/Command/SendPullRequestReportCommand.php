ee4bce82592b1eec477acb0da01c4ef54d26a915
Mickaël Andrieu <andrieu.travail@gmail.com>
2016-06-13 15:27:52 +0000

    bootstrapped mailing report command + mailcatcher config

---
+++ src/AppBundle/Command/SendPullRequestReportCommand.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace AppBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+/**
+ * 
+ */
+class SendPullRequestReportCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('pull_request:report:send_mail')
+            ->setDescription('Send Pull request by mail')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new SymfonyStyle($input, $output);
+        $groups = $this->getContainer()->getParameter('recipients');
+        $reporter = $this->getContainer()->get('app.pull_request.reporter');
+        $mailer = $this->getContainer()->get('app.mailer');
+        $nbMails = 0;
+
+        $io->title('Pull requests Reporter');
+        $io->comment('List of recipients');
+        $headers = ['Group', 'Emails'];
+        $io->table($headers, $this->getRows($groups));
+
+        try {
+            foreach ($groups as $groupName => $groupMembers) {
+                foreach ($groupMembers as $groupMember) {
+                    $nbMails += $mailer->send(
+                        'Daily report '.date('d/m/Y'),
+                        'mickael.andrieu@prestashop.com',
+                        $groupMember,
+                        'mail/pr_sumup_for_mail.html.twig',
+                        $reporter->reportActivity()
+                    );
+                }
+            }
+        } catch (\Exception $e) {
+            $io->error($e->getMessage());
+        }
+
+        $io->success($nbMails.' mails successfuly sent !');
+    }
+
+    private function getRows($groups)
+    {
+        $rows = [];
+
+        foreach ($groups as $groupName => $groupMembers) {
+            foreach ($groupMembers as $groupMember) {
+                $rows[] = [$groupName, $groupMember];
+            }
+        }
+
+        return $rows;
+    }
+}
