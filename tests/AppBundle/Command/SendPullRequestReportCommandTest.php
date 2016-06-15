19db600a0f81b6ed991c1114e09de67ca94c4082
Mickaël Andrieu <andrieu.travail@gmail.com>
2016-06-15 16:39:06 +0000

    added tests for command output

---
+++ tests/AppBundle/Command/SendPullRequestReportCommandTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace tests\AppBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+use AppBundle\Command\SendPullRequestReportCommand;
+
+class SendPullRequestReportCommandTest extends WebTestCase
+{
+    protected function setUp()
+    {
+        static::$kernel = static::createKernel();
+        static::$kernel->boot();
+    }
+    
+    public function testExecute()
+    {
+        $application = new Application(static::$kernel);
+        $application->add(new SendPullRequestReportCommand());
+
+        $command = $application->find('pull_request:report:send_mail');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(array('command' => $command->getName()));
+
+        $this->assertRegExp('/Pull requests Reporter/', $commandTester->getDisplay());
+        $this->assertRegExp('/\/\/ List of recipients/', $commandTester->getDisplay());
+        $this->assertRegExp('/waiting for code review/', $commandTester->getDisplay());
+        $this->assertRegExp('/waiting for QA feedback/', $commandTester->getDisplay());
+        $this->assertRegExp('/waiting for PM feedback/', $commandTester->getDisplay());
+    }
+}
