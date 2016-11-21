82f2a787a1974989e8410ed50e576c1eff7d53e1
Mickaël Andrieu <andrieu.travail@gmail.com>
2016-11-21 14:41:02 +0000

    Added functional test

--- tests/AppBundle/Controller/WebhookControllerTest.php
+++ tests/AppBundle/Controller/WebhookControllerTest.php
@@ -69,6 +69,32 @@ class WebhookControllerTest extends WebTestCase
                     'action' => 'commits labels checked', ],
             ],
         ];
+        $tests['Pull request creation with wording'] = [
+            'pull_request',
+            'pull_request_opened_wording.json',
+            [
+                [
+                    'event' => 'pr_opened',
+                    'action' => 'table description checked',
+                ],
+                [
+                    'event' => 'pr_opened',
+                    'action' => 'labels initialized',
+                ],
+                [
+                    'event' => 'pr_opened',
+                    'action' => 'user welcomed',
+                ],
+                [
+                    'event' => 'pr_opened',
+                    'action' => 'checked for new translations',
+                    'status' => 'found',
+                ],
+                [
+                    'event' => 'pr_opened',
+                    'action' => 'commits labels checked', ],
+            ],
+        ];
         $tests['Add labels'] = [
             'issues',
             'issues.labeled.bug.json',
