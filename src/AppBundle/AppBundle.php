8baff162faa419f5f9b2595e148d729b2f83c2f9
Mickaël Andrieu <andrieu.travail@gmail.com>
2016-12-12 14:20:27 +0000

    Upgraded to KnpGitHubClient 2 + refactoring

--- src/AppBundle/AppBundle.php
+++ src/AppBundle/AppBundle.php
@@ -2,15 +2,8 @@
 
 namespace AppBundle;
 
-use AppBundle\DependencyInjection\Compiler\SecureApplicationPass;
-use Symfony\Component\DependencyInjection\Compiler\PassConfig;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class AppBundle extends Bundle
 {
-    public function build(ContainerBuilder $container)
-    {
-        $container->addCompilerPass(new SecureApplicationPass(), PassConfig::TYPE_BEFORE_REMOVING, 10);
-    }
 }
