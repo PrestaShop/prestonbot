eec05db0406c9ba664361dc6b552ab398b4f16d6
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2018-07-05 18:01:07 +0200

    Prevent label changes
    If label has a new name, we need to check for aliases.

--- src/AppBundle/PullRequests/Labels.php
+++ src/AppBundle/PullRequests/Labels.php
@@ -12,4 +12,17 @@ final class Labels
     const WAITING_FOR_QA_FEEDBACK = 'waiting for QA feedback';
 
     const WAITING_FOR_PM_FEEDBACK = 'waiting for PM feedback';
+
+    const FEATURE = 'Feature';
+
+    const BUG = 'Bug';
+
+    const IMPROVEMENT = 'Improvement';
+
+    // Help to prevent changes in the future
+    const ALIASES = [
+        'new feature' => self::FEATURE,
+        'bug fix' => self::BUG,
+        'improvement' => self::IMPROVEMENT,
+    ];
 }
