75604ffbfaf798fd452e2d4ced021abd593bd85e
Jonathan Lelievre <jonathan.lelievre@prestashop.com>
2023-07-03 13:27:35 +0200

    Fix more deprecations

--- src/AppBundle/Twig/Extension/AppExtension.php
+++ src/AppBundle/Twig/Extension/AppExtension.php
@@ -4,19 +4,13 @@ namespace AppBundle\Twig\Extension;
 
 use Twig\Environment;
 use Twig\Extension\AbstractExtension;
-use Twig\TwigFilter;
-use Twig\TwigFunction;
 
 class AppExtension extends AbstractExtension
 {
     public function __construct(Environment $environment)
     {
-        $environment->registerUndefinedFunctionCallback(function ($name) {
-            return new TwigFunction($name, [$this, 'doNothing']);
-        });
-        $environment->registerUndefinedFilterCallback(function ($name) {
-            return new TwigFilter($name, [$this, 'doNothing']);
-        });
+        $environment->registerUndefinedFunctionCallback(function () {});
+        $environment->registerUndefinedFilterCallback(function () {});
     }
 
     public function doNothing()
